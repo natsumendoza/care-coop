d314228c87713c63a1a8fa14f90ff4c1bed31925
natsumendoza <mendozalaxus@yahoo.com>
2016-06-05 09:04:18 +0800

    rename package name

--- src/main/java/org/care/coop/ws/controllers/HomeController.java
+++ src/main/java/org/care/coop/ws/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package org.care.coop.ws.controller;
+package org.care.coop.ws.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
